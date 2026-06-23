b7fb2efba71b260b54f791eaa0a334d50ecdafc7
Rajjjj06 <bhavyarajsingh88@gmail.com>
2026-06-23 13:08:12 +0530

    added k8s and fixed the migrate script in blog-service

--- blog-service/migrate.js
+++ blog-service/migrate.js
@@ -1,11 +1,23 @@
 import fs from "fs";
 import pool from "./config/db.js";
 
-const migrations = ["./migrations/comments.sql"];
+const migrations = [
+  "./migrations/user_profile.sql",
+  "./migrations/blog_category.sql",
+  "./migrations/blog.sql",
+  "./migrations/comments.sql",
+];
 
-for (const file of migrations) {
-  const migrate = fs.readFileSync(file, "utf-8");
-  await pool.query(migrate);
-  console.log("Migration completed successfully");
+try {
+  for (const file of migrations) {
+    const sql = fs.readFileSync(file, "utf-8");
+    await pool.query(sql);
+    console.log(`${file} migrated successfully`);
+  }
+
+  console.log("All migrations completed successfully");
   process.exit(0);
+} catch (error) {
+  console.error("Migration failed:", error);
+  process.exit(1);
 }
